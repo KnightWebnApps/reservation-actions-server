9075950c8c43624f591e23b3a2c79d7d9b0736d4
MyMac <mymac@MacBook-Pro-8.local>
2020-08-24 15:07:12 -0400

    fixed insert reservation mutation

--- src/handlers/validateReservation.js
+++ src/handlers/validateReservation.js
@@ -48,11 +48,13 @@ const validateReservation = async (req, res) => {
       { appointment, name, date, serviceId, userId },
       INSERT_RESERVATION
     );
+
+    console.log(insertReservations.errors);
   
-    if (insertReservations.errors !== null || insertReservations.errors !== undefined) {
-      console.log(errors)
+    if (!insertReservations.errors) {
+      console.log(insertReservations.errors);
       return res.status(400).json({
-        message: "Error: " + insertReservations.errors[0].message,
+        message: "Something happened",
       });
     } else {
       res.status(200).json({
